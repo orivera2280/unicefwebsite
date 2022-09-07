c5c92f0977d3cf5296a8705b13e8933cb4db71b3
Oscar Rivera <24orivera@woodward.edu>
2022-09-06 21:40:28 -0400

    d

--- Desktop/unicefwebsite/app.py
+++ Desktop/unicefwebsite/app.py
@@ -1,4 +1,11 @@
+import os
 from flask import Flask, render_template, request, session, redirect
+from flask_session import Session
+from functools import wraps
+from cs50 import SQL
+from werkzeug.security import check_password_hash, generate_password_hash
+import psycopg2
+from flask_mail import Mail, Message
 
 @app.route("index.html")
 def index():
