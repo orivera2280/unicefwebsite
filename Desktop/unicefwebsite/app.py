41f61573ae2f271f0fff70b89a2f971511a0d099
Oscar Rivera <24orivera@woodward.edu>
2022-09-06 21:42:36 -0400

    d

--- Desktop/unicefwebsite/app.py
+++ Desktop/unicefwebsite/app.py
@@ -7,6 +7,6 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import psycopg2
 from flask_mail import Mail, Message
 
-@app.route("index.html")
+@app.route("/index.html")
 def index():
     return render_template("index.html")
